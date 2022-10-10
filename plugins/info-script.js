310f76bc39f6877e0b49e5c587aff202466cda41
LEVI-GANZ-BOTZ <111663350+mytobot@users.noreply.github.com>
2022-10-10 19:47:56 +0800

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow*
+let str = `*https://chat.whatsapp.com/JkdB3gSCwCRGgOdCRfkAt1*
 
 Pengen mentahan nya ?
 Nih ada santai bro....
@@ -18,10 +18,10 @@ wa.me/6282127487538`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://instagram.com/imleviofc",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    title: 'LEVI-MODS-GANZ MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
